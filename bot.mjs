733ecd62642db059caf3b72f0367e5514e572890
Ruslan Tregubchenko <okeyff@squadvel.com>
2023-02-09 21:20:52 +0300

    Fix duplicated answers for text

--- bot.mjs
+++ bot.mjs
@@ -2,7 +2,7 @@ import TeleBot from 'telebot';
 
 const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN);
 
-bot.on('text', (msg) => msg.reply.text(msg.text));
+bot.on('text', (msg) => (msg.text.startsWith('/') ? null : msg.reply.text(msg.text)));
 
 bot.on('/start', (msg) => msg.reply.photo('https://picsum.photos/1000'));
 
