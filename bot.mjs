ef6bc98ab60645687139323c57a2c4becd2240f7
Ruslan Tregubchenko <okeyff@squadvel.com>
2023-02-09 21:16:04 +0300

    added test handler

--- bot.mjs
+++ bot.mjs
@@ -1,7 +1,9 @@
-import TeleBot from "telebot"
+import TeleBot from 'telebot';
 
-const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN)
+const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN);
 
-bot.on('text', msg => msg.reply.text(msg.text))
+bot.on('text', (msg) => msg.reply.text(msg.text));
 
-export default bot
+bot.on('/start', (msg) => msg.reply.photo('https://picsum.photos/1000'));
+
+export default bot;
